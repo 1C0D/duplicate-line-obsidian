927aa3d75ab04de30935b21556a28b5881b1200c
1C0D <1conscience@gmx.fr>
2024-01-01 20:49:48 +0100

    dev change

---
+++ scripts/start.mjs
@@ -0,0 +1,7 @@
+import { execSync } from 'child_process';
+
+execSync("start /B code .", { stdio: "ignore", shell: true });
+execSync('git fetch'); // avoid an error on pull
+execSync('git pull')
+execSync('npm install', { stdio: 'inherit' });
+execSync('npm run dev', { stdio: 'inherit' });
--- scripts/start.mts
+++
@@ -1,6 +0,0 @@
-import { execSync } from 'child_process';
-
-execSync("start /B code .", { stdio: "ignore", shell: "true" });
-execSync('npm install', { stdio: 'inherit' });
-execSync('npm run dev', { stdio: 'inherit' });
-
