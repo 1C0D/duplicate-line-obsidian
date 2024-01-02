6ae691ce2ecb8a53a8b620efe3e2dff06ea7ee25
1C0D <1conscience@gmx.fr>
2024-01-02 19:12:11 +0100

    dev

--- inject_scripts/scripts/start.mjs
+++
@@ -1,7 +0,0 @@
-import { execSync } from 'child_process';
-
-execSync("start /B code .", { stdio: "ignore", shell: "true" });
-execSync('git fetch'); // avoid an error on pull
-execSync('git pull')
-execSync('npm install', { stdio: 'inherit' });
-execSync('npm run dev', { stdio: 'inherit' });
